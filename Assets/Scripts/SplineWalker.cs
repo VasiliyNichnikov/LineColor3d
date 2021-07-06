ffbec95955894a79555bfd9418c1807c652ac3fd
Vasiliy <v.nichnikov@gmail.com>
2021-07-06 17:58:24 +0300

    Инициализация проекта

---
+++ Assets/Scripts/SplineWalker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class SplineWalker : MonoBehaviour
+{
+    [SerializeField] private BezierSpline _spline;
+    [SerializeField] private float _duration;
+    private float _progress;
+    private Transform _thisTransform;
+
+    private void Start()
+    {
+        _thisTransform = transform;
+    }
+
+    private void Update()
+    {
+        _progress += Time.deltaTime / _duration;
+        if (_progress > 1f)
+        {
+            _progress = 1f;
+        }
+        _thisTransform.localPosition = _spline.GetPoint(_progress);
+    }
+}
