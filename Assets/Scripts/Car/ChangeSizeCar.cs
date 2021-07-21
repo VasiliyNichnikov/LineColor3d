59e3502fa6ef94b353c547c982487437da0d2887
Vasiliy <57954863+VasiliyNichnikov@users.noreply.github.com>
2021-07-21 18:45:08 +0300

    Изменение размера машины

--- Assets/Scripts/Car/ChangeSizeCar.cs
+++ Assets/Scripts/Car/ChangeSizeCar.cs
@@ -1,14 +1,33 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ChangeSizeCar : MonoBehaviour
 {
-    [SerializeField] private float _hValue = 0.0f;
-    [SerializeField] private float _wValue = 0.0f;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private AnimationClip _clipHeight, _clipWidth;
+    
+    private float _hValue;
+    private float _wValue;
+
+    private void Start()
+    {
+        print(_clipHeight.length);
+    }
 
     private void OnGUI()
     {
-        _wValue = GUI.HorizontalSlider(new Rect(Screen.width / 2 - 90, 10, 180, 20), _wValue, 0.0f, 1.0f);
         _hValue = GUI.VerticalSlider(new Rect(0, Screen.height / 2 - 150, 20, 300), _hValue, 1.0f, 0.0f);
+        _wValue = GUI.HorizontalSlider(new Rect(Screen.width / 2 - 90, 10, 180, 20), _wValue, 0.0f, 1.0f);
     }
+
+    private void Update()
+    {
+        PlayClipFromFrame(_clipHeight, 1, _hValue);
+        PlayClipFromFrame(_clipWidth, 2, _wValue);
+    }
+
+    private void PlayClipFromFrame(AnimationClip clip, int layer, float time)
+    {
+        _animator.Play(clip.name, layer, time);
+    }
+    
 }
